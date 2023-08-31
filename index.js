c1f9c3d25ed851222d409ef14afca66712d1e980
Junior Teixeira <teixeirajunior19051988@gmail.com>
2023-08-30 21:48:38 -0300

    Reajuste no código para transformar String rm Number

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
+    let entradaDeDivida = document.querySelector("#divida");
 
-
-function valorDaDivida() {
-    divida.blur();
-    divida.value = prompt("Insira o Valor da Dívida");
-    diasDeAtraso() 
+function valorDaDivida(form) {
+     form.divida.blur();
+     entradaDeDivida = parseInt(prompt("Insira o Valor da Dívida"));
+    diasDeAtraso(form) 
 }
 
-function diasDeAtraso() {
-    atraso.blur();
-    atraso.value = prompt("Insira os dias de atraso");
-    taxaJuros();
+function diasDeAtraso(form) {
+     form.atraso.blur();
+     form.atraso.value = prompt("Insira os dias de atraso");
+    taxaJuros(form);
 }
 
-function taxaJuros() {
-    juros.blur();
-    if(atraso.value <= 15){
-        juros.value = "Taxa de 5% de Juros."
-    }else if(atraso.value > 15){
-        juros.value = "Taxa de 10% de Juros."
+function taxaJuros(form) {
+    form.juros.blur();
+    if(form.atraso.value <= 15){
+        form.juros.value = "Taxa de 5% de Juros."
+    }else if(form.atraso.value > 15){
+        form.juros.value = "Taxa de 10% de Juros."
     }else{
-        juros.value = "Valor Normal."
+        form.juros.value = "Valor Normal."
     }
-    valorDoResultado();
+    valorDoResultado(form);
 }
 
-function valorDoResultado() {
-
-    if(divida.value > 0){
-        if(atraso.value > 0){
-            if(atraso.value <= 15){
-                resultado.value = divida.value + divida.value * 0.5 * 0.10;
-            } else if(atraso.value > 15) {
-                resultado.value = divida.value + divida.value * 0.10 * 0.10;
+function valorDoResultado(form) { 
+     form.resultado.blur();
+    if(entradaDeDivida > 0){
+        if(form.atraso.value > 0){
+            if(form.atraso.value <= 15){
+                form.resultado.value =   entradaDeDivida + entradaDeDivida * 0.5 * 0.10 + ",00 de Dívida";
+            } else if(form.atraso.value > 15) {
+                form.resultado.value = entradaDeDivida + entradaDeDivida * 0.10 * 1.00 + ",00 de Dívida";
             } else{
                 return "fim"
             }
@@ -44,20 +44,23 @@ function valorDoResultado() {
 
     //J = C*i*t (J juros, C capital, i taxa de juros e t tempo);
     //5% de 1.000, a conta fica assim: (5 x 1.000) ÷ 100
+    console.log(typeof entradaDeDivida)
 }
 const btn = document.querySelector("#btn");
 
 btn.addEventListener('click' , function() {
      let entradaDivida = document.querySelector("#divida");
      let entradaAtraso = document.querySelector("#atraso");
-     let entradaJuros = document.querySelector("#juros");
-     let entradaResultado = document.querySelector("#resultado")
+     let entradaJuros =  document.querySelector("#juros");
+     let entradaResultado = document.querySelector("#resultado");
 
      entradaDivida.value = "";
      entradaAtraso.value = "";
      entradaJuros.value = ""
      entradaResultado.value = "";
 })
+
+
 
 
 
